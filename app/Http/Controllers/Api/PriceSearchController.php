a36e4669aefa063cd4c9229c0b9de6eb02ee70e0
gasseraly <gasser010gasser@gmail.com>
2025-11-29 06:53:26 +0200

    fix: remove rounding from price_statistics average_price

--- app/Http/Controllers/Api/PriceSearchController.php
+++ app/Http/Controllers/Api/PriceSearchController.php
@@ -602,7 +602,7 @@ class PriceSearchController extends BaseApiController
                 'price_statistics' => [
                     'lowest_price' => (float) $lowestPrice,
                     'highest_price' => (float) $highestPrice,
-                    'average_price' => round((float) $averagePrice, 2),
+                    'average_price' => (float) $averagePrice,
                     'price_range' => (float) ($highestPrice - $lowestPrice),
                     'savings_amount' => (float) ($highestPrice - $lowestPrice),
                 ],
